9168c9e5ea8733f0d5dab009e441b66afe75c1e1
junyeong-nero <magicwho@unist.ac.kr>
2026-03-25 17:21:51 +0900

    [refactor] remove HTML visualizer and visualize CLI command

--- src/tiny_graph_rag/visualization/__init__.py
+++ src/tiny_graph_rag/visualization/__init__.py
@@ -1,6 +1,5 @@
 """Visualization module for knowledge graphs."""
 
-from .html_visualizer import HtmlVisualizer
 from .pyvis_visualizer import PyVisVisualizer
 
-__all__ = ["HtmlVisualizer", "PyVisVisualizer"]
+__all__ = ["PyVisVisualizer"]
